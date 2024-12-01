ffe72759cdc801bd7158ca136b7c63c1de8532da
Tim Beck <becktimd@gmail.com>
2024-12-01 08:29:44 -0800

    init

---
+++ lib/aoc24.ex
@@ -0,0 +1,18 @@
+defmodule Aoc24 do
+  @moduledoc """
+  Documentation for `Aoc24`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Aoc24.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/aoc24_test.exs
@@ -0,0 +1,8 @@
+defmodule Aoc24Test do
+  use ExUnit.Case
+  doctest Aoc24
+
+  test "greets the world" do
+    assert Aoc24.hello() == :world
+  end
+end
