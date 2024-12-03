314384b051c0aac6eb6b8aa9f50a4303ab4eef6c
Timothy Beck <tbeck@riotgames.com>
2024-12-03 13:16:50 -0800

    windows

--- lib/day3.ex
+++ lib/day3.ex
@@ -1,5 +1,4 @@
 defmodule Aoc24.Day3 do
-
   def pattern do
     ~r/mul\((\d{0,3}),(\d{0,3})\)/
   end
@@ -10,8 +9,21 @@ defmodule Aoc24.Day3 do
     |> Enum.reduce(0, &+/2)
   end
 
-
   def part2(input) do
+    do_indexes = indexes_of(input, ~r/do\(\)/)
+    dont_indexes = indexes_of(input, ~r/don't\(\)/)
+    IO.inspect(do_indexes, charlists: :as_lists)
+    IO.inspect(dont_indexes, charlists: :as_lists)
     0
+  end
+
+  def indexes_of(input, pat) do
+    Regex.scan(pat, input, return: :index)
+    |> Enum.map(fn [tuple] -> {index, _} = tuple; index end)
+  end
+
+  def in_range(index, indexes) do
+    [start_i, end_i] = indexes
+    index > start_i and index < end_i
   end
 end
--- test/aoc24_test.exs
+++ test/aoc24_test.exs
@@ -147,7 +147,7 @@ defmodule Aoc24Test do
     end
 
     test "day 3 part 2 example", %{test: test_name} do
-      input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+      input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 
       output =
         input
