32cca5b901bd8cc68a878bca93d75b7514aaf139
Timothy Beck <tbeck@riotgames.com>
2024-12-03 12:44:24 -0800

    day 3 test case

---
+++ lib/day3.ex
@@ -0,0 +1,7 @@
+defmodule Aoc24.Day3 do
+
+  def part1(input) do
+    input
+  end
+
+end
--- test/aoc24_test.exs
+++ test/aoc24_test.exs
@@ -94,32 +94,56 @@ defmodule Aoc24Test do
     end
 
     test "all inc or dec" do
-      assert Aoc24.Day2.all_inc_or_dec([1,2,3,4,5])
-      assert Aoc24.Day2.all_inc_or_dec([5,4,3,2,1])
-      assert Aoc24.Day2.all_inc_or_dec([5,4,3,2,0])
+      assert Aoc24.Day2.all_inc_or_dec([1, 2, 3, 4, 5])
+      assert Aoc24.Day2.all_inc_or_dec([5, 4, 3, 2, 1])
+      assert Aoc24.Day2.all_inc_or_dec([5, 4, 3, 2, 0])
       assert Aoc24.Day2.all_inc_or_dec([1])
-      refute Aoc24.Day2.all_inc_or_dec([1,10,11,12,11])
+      refute Aoc24.Day2.all_inc_or_dec([1, 10, 11, 12, 11])
     end
 
     test "differences ok" do
-      assert Aoc24.Day2.differences_ok([1,2,3,4,5])
-      assert Aoc24.Day2.differences_ok([1,2,3,4,6])
-      assert Aoc24.Day2.differences_ok([1,2,3,4,7])
+      assert Aoc24.Day2.differences_ok([1, 2, 3, 4, 5])
+      assert Aoc24.Day2.differences_ok([1, 2, 3, 4, 6])
+      assert Aoc24.Day2.differences_ok([1, 2, 3, 4, 7])
       assert Aoc24.Day2.differences_ok([1])
-      refute Aoc24.Day2.differences_ok([1,2,3,4,8])
-      refute Aoc24.Day2.differences_ok([1,10,11,12])
+      refute Aoc24.Day2.differences_ok([1, 2, 3, 4, 8])
+      refute Aoc24.Day2.differences_ok([1, 10, 11, 12])
     end
 
     test "differences ok with removal" do
-      assert Aoc24.Day2.ok_with_deletes([1,2,3,4,5])
-      assert Aoc24.Day2.ok_with_deletes([1,2,3,4,6])
-      assert Aoc24.Day2.ok_with_deletes([1,2,3,4,7])
+      assert Aoc24.Day2.ok_with_deletes([1, 2, 3, 4, 5])
+      assert Aoc24.Day2.ok_with_deletes([1, 2, 3, 4, 6])
+      assert Aoc24.Day2.ok_with_deletes([1, 2, 3, 4, 7])
       assert Aoc24.Day2.ok_with_deletes([1])
-      assert Aoc24.Day2.ok_with_deletes([1,2,3,4,8])
-      assert Aoc24.Day2.ok_with_deletes([1,10,11,12])
-      refute Aoc24.Day2.ok_with_deletes([1,10,11,12,20])
-      refute Aoc24.Day2.ok_with_deletes([1,2,7,8,9])
-      refute Aoc24.Day2.ok_with_deletes([9,7,6,2,1])
+      assert Aoc24.Day2.ok_with_deletes([1, 2, 3, 4, 8])
+      assert Aoc24.Day2.ok_with_deletes([1, 10, 11, 12])
+      refute Aoc24.Day2.ok_with_deletes([1, 10, 11, 12, 20])
+      refute Aoc24.Day2.ok_with_deletes([1, 2, 7, 8, 9])
+      refute Aoc24.Day2.ok_with_deletes([9, 7, 6, 2, 1])
+    end
+  end
+
+  describe "Day 3" do
+    test "day 3 part 1 example", %{test: test_name} do
+      input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+
+      output =
+        input
+        |> Aoc24.Day3.part1()
+        |> print_out(test_name)
+
+      assert output == 161
+    end
+
+    test "day 3 part 1 input", %{test: test_name} do
+      {:ok, contents} = File.read("./test/support/day3/input_part1.txt")
+
+      output =
+        contents
+        |> Aoc24.Day3.part1()
+        |> print_out(test_name)
+
+      assert output == 1_000_000
     end
   end
 
@@ -127,5 +151,4 @@ defmodule Aoc24Test do
     IO.puts("#{Atom.to_string(test)}:\t#{Integer.to_string(output)}")
     output
   end
-
 end
