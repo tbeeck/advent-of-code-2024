edb06aa043ab48991fface50b4ea622e82cb7a05
Tim Beck <becktimd@gmail.com>
2024-12-20 10:41:42 +0100

    grid

--- lib/day20.ex
+++ lib/day20.ex
@@ -1,10 +1,33 @@
 defmodule Aoc24.Day20 do
+  alias Aoc24.Util
+
   def part1(input) do
+    grid =
+      process_input(input)
+      |> IO.inspect(width: 200)
+
+    start_point = Util.find_first_in_2d_tuple(grid, :start)
+    end_point = Util.find_first_in_2d_tuple(grid, :end)
     0
   end
 
-
   def process_input(input) do
-    input
+    grid =
+      String.split(input)
+      |> Enum.map(fn row ->
+        String.graphemes(row)
+        |> Enum.map(fn char ->
+          case char do
+            "#" -> :wall
+            "." -> :free
+            "S" -> :start
+            "E" -> :end
+          end
+        end)
+        |> List.to_tuple()
+      end)
+      |> List.to_tuple()
+
+    grid
   end
 end
--- lib/util/util.ex
+++ lib/util/util.ex
@@ -15,6 +15,12 @@ defmodule Aoc24.Util do
     0 <= x and x < width and 0 <= y and y < height
   end
 
+  @spec valid_neighbors({integer(), integer()}, any()) :: [{integer(), integer()}, ...]
+  def valid_neighbors(origin, dimensions) do
+    neighbors_of(origin)
+    |> Enum.filter(fn n -> in_bounds?(n, dimensions) end)
+  end
+
   @spec directions() :: [:down | :left | :right | :up, ...]
   def directions() do
     [
@@ -36,5 +42,36 @@ defmodule Aoc24.Util do
       end
 
     {x + dx, y + dy}
+  end
+
+  @spec find_first_in_2d_tuple(tuple(), any()) :: {integer(), integer()} | nil
+  def find_first_in_2d_tuple(grid, value) do
+    {width, height} = tuple_dimensions_2d(grid)
+
+    vals =
+      for x <- 0..(width - 1),
+          y <- 0..(height - 1),
+          grid_val(grid, {x, y}) == value,
+          do: {x, y}
+
+    List.first(vals, nil)
+  end
+
+  @spec tuple_dimensions_2d(tuple()) :: {integer(), integer()}
+  def tuple_dimensions_2d(grid) do
+    height = tuple_size(grid)
+    width = tuple_size(elem(grid, 0))
+    {width, height}
+  end
+
+  @spec grid_val(tuple(), {integer(), integer()}) :: any() | nil
+  def grid_val(grid, {x, y}) do
+    dims = tuple_dimensions_2d(grid)
+
+    if in_bounds?({x, y}, dims) do
+      elem(elem(grid, y), x)
+    else
+      nil
+    end
   end
 end
