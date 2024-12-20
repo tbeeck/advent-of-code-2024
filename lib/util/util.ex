2a272555872967c73e09bcf036d26cae63d3b4b8
Tim Beck <becktimd@gmail.com>
2024-12-20 10:20:31 +0100

    direction helpers

--- lib/util/util.ex
+++ lib/util/util.ex
@@ -5,17 +5,36 @@ defmodule Aoc24.Util do
   end
 
   @spec neighbors_of({integer(), integer()}) :: [{integer(), integer()}, ...]
-  def neighbors_of({x, y}) do
-    [
-      {x + 1, y},
-      {x, y + 1},
-      {x - 1, y},
-      {x, y - 1}
-    ]
+  def neighbors_of(origin) do
+    directions()
+    |> Enum.map(fn d -> new_position(origin, d) end)
   end
 
   @spec in_bounds?({integer(), integer()}, {integer(), integer()}) :: boolean()
   def in_bounds?({x, y}, {width, height}) do
     0 <= x and x < width and 0 <= y and y < height
+  end
+
+  @spec directions() :: [:down | :left | :right | :up, ...]
+  def directions() do
+    [
+      :up,
+      :right,
+      :down,
+      :left
+    ]
+  end
+
+  @spec new_position({number(), number()}, :down | :left | :right | :up) :: {number(), number()}
+  def new_position({x, y}, direction) do
+    {dx, dy} =
+      case direction do
+        :up -> {0, -1}
+        :down -> {0, 1}
+        :left -> {-1, 0}
+        :right -> {1, 0}
+      end
+
+    {x + dx, y + dy}
   end
 end
